e824502db234b634bc6dbeea4b16939fdbc4af58
Gaurav Shukla <gaurav_shukla@mckinsey.com>
2019-08-31 11:35:47 +0530

    updated pom and the application java file

--- src/main/java/parkingLot/Application.java
+++ src/main/java/parkingLot/Application.java
@@ -3,6 +3,7 @@ package parkingLot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
@@ -11,12 +12,14 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Main initialization application class
  */
+@EnableDiscoveryClient
+//@Import(AccountsConfiguration.class)
+
 @Configuration
 @ComponentScan
 @EnableJpaRepositories
